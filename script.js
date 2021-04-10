e137c4fb43023763bb787ee6f0d0b8159c316b8f
Jimin L <69053105+chocothelee@users.noreply.github.com>
2021-04-11 01:53:26 +0800

    Update script.js
    - Added candle and candle flame's visibility into birthday function
    
    - Added candle flame function

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 var button = document.getElementById("but");
 var text = document.getElementById("text");
-
-button.addEventListener("click", birthday);
+var ptext = document.getElementById("ptext");
+var candle = document.getElementById("can");
+var flame = document.getElementById("flame");
 
 function birthday() {
 	button.style.setProperty("font-size", "100px");
 	button.style.setProperty("border-radius", "100px 100px 50px 50px");
-	button.style.setProperty("transform", " translate(-25px, 0px)");
 	button.innerText = "CAKE";
 	button.style.setProperty("background-color", "#FC7A6F");
 	button.style.setProperty("color", "#FC7A6F");
 	button.style.setProperty("border", "10px solid #FC7A6F");
 
 	text.style.visibility = "visible";
+	ptext.style.visibility = "visible";
+	candle.style.visibility = "visible";
+	flame.style.visibility = "visible";
+}
+
+function candlefl() {
+	flame.style.visibility = "hidden";
 }
